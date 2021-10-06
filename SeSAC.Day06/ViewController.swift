14cdfea9d0d8a6f8f2a73d069926ff3d637d14df
Teiresias22 <teiresias1987@gmail.com>
2021-10-06 09:06:57 +0900

    Initial Commit

---
+++ SeSAC.Day06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC.Day06
+//
+//  Created by Joonhwan Jeon on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

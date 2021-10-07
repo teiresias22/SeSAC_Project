92fc3ad10eb0900f9bdac21f4a9b5e8f6ef462ff
Teiresias22 <teiresias1987@gmail.com>
2021-10-07 23:15:28 +0900

    DatePicker and D-day Update

--- SeSAC.Day06/Assets.xcassets/EctViewController.swift
+++ SeSAC.Day06/Assets.xcassets/EctViewController.swift
@@ -3,30 +3,37 @@ import UIKit
 class EctViewController: UIViewController {
     @IBOutlet var datePicker: UIDatePicker!
     
+    @IBOutlet var datePicker100: UILabel!
+    @IBOutlet var datePicker200: UILabel!
+    @IBOutlet var datePicker300: UILabel!
+    @IBOutlet var datePicker400: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if #available(iOS 14.0, *) {
         datePicker.preferredDatePickerStyle = .inline
+        }else {
+            datePicker.preferredDatePickerStyle = .wheels
         }
-        
         
     }
     @IBAction func datePickerValueChange(_ sender: UIDatePicker) {
+        let dateFormat = DateFormatter()
         
-        print(datePicker.date)
-        print(sender.date)
+        dateFormat.locale = Locale(identifier: "ko_KR")
+        dateFormat.dateFormat = "yyyy년 \n MM월 dd일"
         
-        //DateFormatter: 1. 원하는 형태대로 날짜를 표기 2. 타임존에 맞춰서 시간 설정
-        let format = DateFormatter()
+        let afterDate100 = Date(timeInterval: 86400 * 100, since: sender.date)
+        let afterDate200 = Date(timeInterval: 86400 * 200, since: sender.date)
+        let afterDate300 = Date(timeInterval: 86400 * 300, since: sender.date)
+        let afterDate400 = Date(timeInterval: 86400 * 400, since: sender.date)
         
-        format.dateFormat = "yyyy년 MM월 dd일" //21/10/20
-        let value = format.string(from: sender.date)
-        print(value)
-        
-        //100일 뒤: TimeInterval
-        let afterDate = Date(timeInterval: 86400 * 100, since: sender.date)
-        print(afterDate)
+        datePicker100.text = dateFormat.string(from: afterDate100)
+        datePicker200.text = dateFormat.string(from: afterDate200)
+        datePicker300.text = dateFormat.string(from: afterDate300)
+        datePicker400.text = dateFormat.string(from: afterDate400)
         
     }
     

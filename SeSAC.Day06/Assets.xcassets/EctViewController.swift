4815ebf0cf72611a923cd5aec9afc2231d23fd0f
Teiresias22 <teiresias1987@gmail.com>
2021-10-07 11:50:20 +0900

    DatePicker Update

--- SeSAC.Day06/Assets.xcassets/EctViewController.swift
+++ SeSAC.Day06/Assets.xcassets/EctViewController.swift
@@ -1,11 +1,33 @@
 import UIKit
 
 class EctViewController: UIViewController {
-
+    @IBOutlet var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if #available(iOS 14.0, *) {
+        datePicker.preferredDatePickerStyle = .inline
+        }
+        
+        
+    }
+    @IBAction func datePickerValueChange(_ sender: UIDatePicker) {
+        
+        print(datePicker.date)
+        print(sender.date)
+        
+        //DateFormatter: 1. 원하는 형태대로 날짜를 표기 2. 타임존에 맞춰서 시간 설정
+        let format = DateFormatter()
+        
+        format.dateFormat = "yyyy년 MM월 dd일" //21/10/20
+        let value = format.string(from: sender.date)
+        print(value)
+        
+        //100일 뒤: TimeInterval
+        let afterDate = Date(timeInterval: 86400 * 100, since: sender.date)
+        print(afterDate)
+        
     }
     
 

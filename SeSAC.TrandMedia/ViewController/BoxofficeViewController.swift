4ac88b52b5fd575a763afb6af51e19c1cc11ce79
Teiresias22 <teiresias1987@gmail.com>
2021-11-08 02:29:30 +0900

    Realm Data Seve [nowDate Error 수정] , [저장된 데이터 활용 안됨]

--- SeSAC.TrandMedia/ViewController/BoxofficeViewController.swift
+++ SeSAC.TrandMedia/ViewController/BoxofficeViewController.swift
@@ -9,10 +9,11 @@ class BoxofficeViewController: UIViewController {
     @IBOutlet weak var boxofficeDatePicker: UIDatePicker!
     
     var boxofficeData: [BoxofficeModel] = []
-    var setDate = "20211103"
+    var setDate: String = "20211103"
     
     var taskList: Results<BoxofficeRank>!
     var filterList: Results<BoxofficeRank>!
+    
     let localRealm = try! Realm()
     
     override func viewDidLoad() {
@@ -24,15 +25,18 @@ class BoxofficeViewController: UIViewController {
         let nibName = UINib(nibName: BoxofficeTableViewCell.identifier, bundle: nil)
         boxofficeTableView.register(nibName, forCellReuseIdentifier: BoxofficeTableViewCell.identifier)
         
-        setAttributes()
-        
         taskList = localRealm.objects(BoxofficeRank.self)
         print("Realm is located at:", localRealm.configuration.fileURL!)
         
-        let predicate = NSPredicate(format: "nowData == %@", setDate)
+        setAttributes()
+        checkRealmData()
+    }
+    
+    func checkRealmData() {
+        let predicate = NSPredicate(format: "nowDate == %@", setDate)
         
         if taskList.filter(predicate).count == 0 {
-            fetcMediaData(nowDate: setDate)
+            fetcMediaData(Date: setDate)
             print("처음 불러옴")
         } else {
             let filter = taskList.filter(predicate)
@@ -53,10 +57,6 @@ class BoxofficeViewController: UIViewController {
             }
         }
     }
-    
-    func checkRealmData() {
-    }
-    
     
     func setAttributes() {
         boxofficeDatePicker.preferredDatePickerStyle = .automatic
@@ -85,7 +85,7 @@ class BoxofficeViewController: UIViewController {
     @objc
     func handleDatePicker(_ sender: UIDatePicker) {
         setDate = Date().setDateString(boxofficeDatePicker.date)
-        fetcMediaData(nowDate: setDate)
+        fetcMediaData(Date: setDate)
         boxofficeTitleLabel.text = "\(Date().setYearString(boxofficeDatePicker.date))년 \(Date().setMonthString(boxofficeDatePicker.date))월 \(Date().setDayString(boxofficeDatePicker.date))일 박스오피스"
     }
     
@@ -104,7 +104,7 @@ class BoxofficeViewController: UIViewController {
         }
         let task = BoxofficeRank(rankArray: rankData, movieNmArray: movieNmData, openDtArray: openDtDate, rankOldAndNewArray: rankOldAndNewData, audiAccArray: audiAccData, nowDate: setDate)
         
-        print("saveRealm task", task)
+        //print("saveRealm task", task)
         try! localRealm.write{
             localRealm.add(task)
         }
@@ -178,7 +178,7 @@ extension BoxofficeViewController: UITableViewDelegate, UITableViewDataSource {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
-    func fetcMediaData(nowDate: String) {
+    func fetcMediaData(Date: String) {
         
         boxofficeData = []
         
@@ -189,7 +189,7 @@ extension BoxofficeViewController: UITableViewDelegate, UITableViewDataSource {
                 let movieNm = movie["movieNm"].stringValue
                 let openDt = movie["openDt"].stringValue
                 let rankOldAndNew = movie["rankOldAndNew"].stringValue
-                
+                        
                 let data = BoxofficeModel(rankData: rank, movieNmData: movieNm, openDtData: openDt, rankOldAndNewData: rankOldAndNew, audiAccData: audiAcc)
                 self.boxofficeData.append(data)
             }
@@ -197,5 +197,4 @@ extension BoxofficeViewController: UITableViewDelegate, UITableViewDataSource {
             self.boxofficeTableView.reloadData()
         }
     }
-    //박스오피스 리스트 클릭하면 검색 화면으로 검색어 등록해서 이동이 되도록
 }

ca9a6fa9bb7322d0493c8b9cfe7ede18589e95a2
JoonHwan_Jeon <teiresias1987@gmail.com>
2022-10-16 01:03:33 +0900

    기존 프로젝트 업로드

---
+++ SSAC.Day02/SSAC.Day02/SigneUpViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SigneUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func tapClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    
+    }
+    
+}
+
---
+++ SSAC.Day02/SSAC.Day02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC.Day02
+//
+//  Created by Joonhwan Jeon on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
